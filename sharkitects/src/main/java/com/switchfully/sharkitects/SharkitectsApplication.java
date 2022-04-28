66fe305b950844dbb8b149a5ca67fbd09220c276
Rinaldo <menichettir@hotmail.com>
2022-04-28 11:14:28 +0200

    Setup Java project

---
+++ sharkitects/src/main/java/com/switchfully/sharkitects/SharkitectsApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.sharkitects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkitectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharkitectsApplication.class, args);
+	}
+
+}
---
+++ sharkitects/src/test/java/com/switchfully/sharkitects/SharkitectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.sharkitects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharkitectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
